efad47b659db290297f1fa696deb5a489a365a29
MaCeleste <mceleste.orona@gmail.com>
2022-09-29 09:26:25 +0100

    Transfer method added

--- budget-app.py
+++ budget-app.py
@@ -34,8 +34,20 @@ class Category:
   def get_ledger(self):
     return self.ledger
 
+  def transfer(self, differentCategory, amount):
+    if self.withdraw(amount, 'Transfer to ' + differentCategory.name):
+      differentCategory.deposit(amount, 'Transfer from ' + differentCategory.name)
+      return True
+    else:
+      return False
+
 food = Category('food')
 food.deposit(5, 'initial deposit')
 food.withdraw(3, 'cake')
 print(food.get_ledger())
 print(food.get_balance())
+beauty = Category('beauty')
+beauty.deposit(15, 'initial deposit')
+print(beauty.transfer(food, 3))
+print(beauty.get_ledger())
+print(food.get_ledger())
